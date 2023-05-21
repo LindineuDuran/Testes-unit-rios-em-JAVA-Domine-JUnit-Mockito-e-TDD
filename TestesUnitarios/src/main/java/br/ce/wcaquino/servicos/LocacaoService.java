58cb8bf935d2f5885e3d3eed8ab07943f14fbd41
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2023-05-21 09:39:39 -0300

    Organização dos arquivos de teste

--- TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -1,15 +1,12 @@
 package br.ce.wcaquino.servicos;
 
-import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
-
-import java.util.Date;
-
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.utils.DataUtils;
-import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 public class LocacaoService
 {
@@ -19,34 +16,6 @@ public class LocacaoService
 
         //Salvando a locacao...
         //TODO adicionar método para salvar
-
-        return locacao;
-    }
-
-    @Test
-    public void teste()
-	{
-        //cenário
-        LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Usuario 1");
-		Filme filme = new Filme("Filme 1", 3, 10.00);
-        Locacao locacaoRef = getLocacao(usuario, filme);
-
-        //ação
-		Locacao locacaoTest = service.alugarFilme(usuario, filme);
-
-        //verificação
-        Assert.assertTrue(locacaoTest.getValor() == locacaoRef.getValor());
-        Assert.assertTrue(DataUtils.isMesmaData(locacaoRef.getDataLocacao(), new Date()));
-        Assert.assertTrue(DataUtils.isMesmaData(locacaoRef.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-
-	}
-
-    private static Locacao getDataEntrega(Locacao locacao)
-    {
-        Date dataEntrega = new Date();
-        dataEntrega = adicionarDias(dataEntrega, 1);
-        locacao.setDataRetorno(dataEntrega);
 
         return locacao;
     }
@@ -61,6 +30,15 @@ public class LocacaoService
 
         //Entrega no dia seguinte
         locacao = getDataEntrega(locacao);
+        return locacao;
+    }
+
+    private static Locacao getDataEntrega(Locacao locacao)
+    {
+        Date dataEntrega = new Date();
+        dataEntrega = adicionarDias(dataEntrega, 1);
+        locacao.setDataRetorno(dataEntrega);
+
         return locacao;
     }
 }
---
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -0,0 +1,30 @@
+package br.ce.wcaquino.servicos;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+public class LocacaoServiceTest
+{
+    @Test
+    public void teste()
+    {
+        //cenário
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuario 1");
+        Filme filme = new Filme("Filme 1", 3, 10.00);
+
+        //ação
+        Locacao locacaoTest = service.alugarFilme(usuario, filme);
+
+        //verificação
+        Assert.assertTrue(locacaoTest.getValor() == 10.00);
+        Assert.assertTrue(DataUtils.isMesmaData(locacaoTest.getDataLocacao(), new Date()));
+        Assert.assertTrue(DataUtils.isMesmaData(locacaoTest.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+    }
+}
